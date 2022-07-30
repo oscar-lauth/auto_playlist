2de0decfb891da18b144d055086999a9059ca88c
Oscar Lauth <oscar.lauth@gmail.com>
2022-07-30 01:36:51 -0400

    Generate page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Quiz from './pages/Quiz';
 import Login from './pages/Login';
+import Generate from './pages/Generate';
 
+let answers:string[] = [];
 function App() {
   return (
      <Router>
        <div className="App">
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path='/quiz' element={<Quiz/>}/>
+        <Route path='/quiz' element={<Quiz handleQuizDone={(ans)=>{
+          answers = ans;
+        }}/>}/>
+        <Route path='/generate' element={<Generate params={answers}/>}/>
       </Routes>
     </div>
      </Router>
