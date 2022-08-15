b65e1ab9b58b8173a2f80d04d08f548b157cbf00
Oscar Lauth <oscar.lauth@gmail.com>
2022-08-15 00:20:44 -0400

    Cookies for display name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Quiz from './pages/Quiz';
-import Login from './pages/Login';
+import Home from './pages/Home';
 import Generate from './pages/Generate';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Cookies from 'js-cookie';
 
 let answers:{}[] = [];
 function App() {
+  let displayName = Cookies.get('display_name');
   return (
      <Router>
        <div className="App">
+        <Header displayName={displayName}/>
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/quiz' element={<Quiz handleQuizDone={(ans)=>{
+        <Route path='/' element={<Home/>}/>
+        <Route path='/quiz' element={<Home displayName={displayName}/>}/>
+        {/* <Route path='/quiz' element={<Quiz handleQuizDone={(ans)=>{
           answers = ans;
-        }}/>}/>
+        }}/>}/> */}
         <Route path='/generate' element={<Generate params={answers}/>}/>
       </Routes>
     </div>
