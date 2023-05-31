2220bf587b0994c8cebae72a75b4411cfaea3a41
Oscar Lauth <oscar.lauth@gmail.com>
2023-05-31 04:39:05 -0400

    store user in session

--- backend/api/user_auth.py
+++ backend/api/user_auth.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter, HTTPException, Depends, Cookie
+from fastapi import APIRouter, HTTPException, Depends, Cookie, Request
 from fastapi.responses import RedirectResponse
 import random
 import string
@@ -18,23 +18,27 @@ async def login_user(settings:config.Settings=Depends(config.get_settings)):
     scope = "user-top-read user-read-private user-read-email playlist-modify-private playlist-modify-public"
     q_params = f"client_id={settings.client_id}&response_type=code&redirect_uri={redirect_uri}&state={state}&scope={scope}"
     url = f"https://accounts.spotify.com/authorize?{q_params}"
-    red_response = RedirectResponse(url=url)
-    red_response.set_cookie(key="stored_state",value=state)
-    return red_response
+    redir_response = RedirectResponse(url=url)
+    redir_response.set_cookie(key="stored_state",value=state)
+    return redir_response
 
 @router.get("/callback")
-async def callback(state:str,code:str = None, error:str = None,stored_state:str=Cookie(),settings:config.Settings=Depends(config.get_settings)):
+async def callback(request: Request,state:str,code:str = None, error:str = None,stored_state:str=Cookie(),settings:config.Settings=Depends(config.get_settings)):
     if error=="access_denied":
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=403)
     elif state != stored_state:
         raise HTTPException(status_code=401,detail="state mismatch")
-    response = req_access_token(code,redirect_uri)
-    if "error" in response:
-        raise HTTPException(status_code=400,detail=response["error"])
-    display_name = response["display_name"];
-    red_response = RedirectResponse(url=settings.frontend_url+'/quiz')
-    red_response.set_cookie(key="display_name",value=display_name)
-    return red_response
+    user = req_access_token(code,redirect_uri)
+    request.session["user"] = user.json()
+    if "error" in user:
+        raise HTTPException(status_code=400,detail=user["error"])
+    display_name = user["display_name"];
+    redir_response = RedirectResponse(url=settings.frontend_url+'/quiz')
+    redir_response.set_cookie(key="display_name",value=display_name)
+    return redir_response
     
+
+# maybe a function to grab user_json from request.session and then convert and return as User model
+# this will keep all request passing away from spotify functions
 
 
