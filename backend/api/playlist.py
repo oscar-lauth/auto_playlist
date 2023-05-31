4837dcca7481102104357237d427c830da7926a5
Oscar Lauth <oscar.lauth@gmail.com>
2023-05-31 19:14:10 -0400

    rewrite api endpoints to use get_user util

--- backend/api/playlist.py
+++ backend/api/playlist.py
@@ -1,18 +1,25 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from spotify.api import generate_playlist
 from spotify.api import create_playlist
+from spotify.utils import get_user_from_session
 
 router = APIRouter()
 
 @router.post("/playlist/generate/{p_id}",status_code=201,description="Add songs")
 async def add_songs(request: Request, p_id:str,attributes:dict,size:int=20):
     print(attributes)
-    response = generate_playlist(request,p_id,attributes,size)
+    user = get_user_from_session(request)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User session data not found")
+    response = generate_playlist(user,p_id,attributes,size)
     return response
 
 @router.post("/playlist/{name}",status_code=201,description="Creates new playlist by name")
 async def new_playlist(request: Request, name:str,public:bool):
-    response = create_playlist(request,name,is_public=public)
+    user = get_user_from_session(request)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User session data not found")
+    response = create_playlist(user,name,is_public=public)
     return response
 
 
--- backend/api/songs.py
+++ backend/api/songs.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
+from spotify.utils import get_user_from_session
 from spotify.api import get_recommendations
 from spotify.api import get_top_tracks_raw, get_top_artists_raw
 
@@ -6,17 +7,26 @@ router = APIRouter()
 
 @router.get("/top_songs")
 async def get_top_songs(request: Request):
-    response = get_top_tracks_raw(request)
+    user = get_user_from_session(request)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User session data not found")
+    response = get_top_tracks_raw(user)
     return response
 
 @router.get("/top_artists")
 async def get_top_artists(request: Request):
-    response = get_top_artists_raw(request)
+    user = get_user_from_session(request)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User session data not found")
+    response = get_top_artists_raw(user)
     return response
 
 @router.get("/recs")
 async def get_recs(request: Request):
-    response = get_recommendations(request)
+    user = get_user_from_session(request)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User session data not found")
+    response = get_recommendations(user)
     res=[]
     for track in response["tracks"]:
         a = []
