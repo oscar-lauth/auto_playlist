5c65a8259cf1e00386d48272ef7f4e959d7e511f
Oscar Lauth <oscar.lauth@gmail.com>
2023-05-31 04:38:08 -0400

    pass request param to api functions

--- backend/api/playlist.py
+++ backend/api/playlist.py
@@ -1,18 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from spotify.api import generate_playlist
 from spotify.api import create_playlist
 
 router = APIRouter()
 
 @router.post("/playlist/generate/{p_id}",status_code=201,description="Add songs")
-async def add_songs(p_id:str,attributes:dict,size:int=20):
+async def add_songs(request: Request, p_id:str,attributes:dict,size:int=20):
     print(attributes)
-    response = generate_playlist(p_id,attributes,size)
+    response = generate_playlist(request,p_id,attributes,size)
     return response
 
 @router.post("/playlist/{name}",status_code=201,description="Creates new playlist by name")
-async def new_playlist(name:str,public:bool):
-    response = create_playlist(name,is_public=public)
+async def new_playlist(request: Request, name:str,public:bool):
+    response = create_playlist(request,name,is_public=public)
     return response
 
 
--- backend/api/songs.py
+++ backend/api/songs.py
@@ -1,22 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from spotify.api import get_recommendations
 from spotify.api import get_top_tracks_raw, get_top_artists_raw
 
 router = APIRouter()
 
 @router.get("/top_songs")
-async def get_top_songs():
-    response = get_top_tracks_raw()
+async def get_top_songs(request: Request):
+    response = get_top_tracks_raw(request)
     return response
 
 @router.get("/top_artists")
-async def get_top_artists():
-    response = get_top_artists_raw()
+async def get_top_artists(request: Request):
+    response = get_top_artists_raw(request)
     return response
 
 @router.get("/recs")
-async def get_recs():
-    response = get_recommendations()
+async def get_recs(request: Request):
+    response = get_recommendations(request)
     res=[]
     for track in response["tracks"]:
         a = []
